dbc25fbbc25c2209a9dbcddc8875c10f140e1bf9
Julia <Julia@192.168.1.70>
2012-03-18 21:05:35 -0500

    commit again

--- spring-webservices-samples/annotated-action-endpoint/src/main/java/com/marvelution/samples/spring/ws/annotated/action/endpoint/HelloWorld.java
+++ spring-webservices-samples/annotated-action-endpoint/src/main/java/com/marvelution/samples/spring/ws/annotated/action/endpoint/HelloWorld.java
@@ -2,8 +2,8 @@ package com.marvelution.samples.spring.ws.annotated.action.endpoint;
 
 public interface HelloWorld {
 
-	public static final String SAY_HI_TEXT = "Hi Easter! ";
-	public static final String SAY_HELLO_TEXT = "Hello Easter! ";
+	public static final String SAY_HI_TEXT = "Hi Easter! Again!";
+	public static final String SAY_HELLO_TEXT = "Hello Easter! what? ";
 	
 	public String sayHi(String text);
 
