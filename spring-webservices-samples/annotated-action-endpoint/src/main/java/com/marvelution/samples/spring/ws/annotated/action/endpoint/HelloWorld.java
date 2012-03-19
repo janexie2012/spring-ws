a9d24ca980fe7a0a6bce09156c6a7d49cc18361b
Julia <Julia@192.168.1.70>
2012-03-18 21:01:12 -0500

    my first commit

--- spring-webservices-samples/annotated-action-endpoint/src/main/java/com/marvelution/samples/spring/ws/annotated/action/endpoint/HelloWorld.java
+++ spring-webservices-samples/annotated-action-endpoint/src/main/java/com/marvelution/samples/spring/ws/annotated/action/endpoint/HelloWorld.java
@@ -2,8 +2,8 @@ package com.marvelution.samples.spring.ws.annotated.action.endpoint;
 
 public interface HelloWorld {
 
-	public static final String SAY_HI_TEXT = "Hi There! I'm the SayHi Operation and you send this text: ";
-	public static final String SAY_HELLO_TEXT = "ello There! I'm the SayHello Operation and you send this text: ";
+	public static final String SAY_HI_TEXT = "Hi Easter! ";
+	public static final String SAY_HELLO_TEXT = "Hello Easter! ";
 	
 	public String sayHi(String text);
 
