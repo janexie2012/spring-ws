27f719a643b213d9e54ee96e7af4c67674c0ac36
Jane Xie <xiaojuansail@gmail.com>
2012-03-18 21:48:16 -0500

    5th commit

--- spring-webservices-samples/annotated-action-endpoint/src/main/java/com/marvelution/samples/spring/ws/annotated/action/endpoint/HelloWorld.java
+++ spring-webservices-samples/annotated-action-endpoint/src/main/java/com/marvelution/samples/spring/ws/annotated/action/endpoint/HelloWorld.java
@@ -3,7 +3,7 @@ package com.marvelution.samples.spring.ws.annotated.action.endpoint;
 public interface HelloWorld {
 
 	public static final String SAY_HI_TEXT = "Hi Easter! All are cleaned up!";
-	public static final String SAY_HELLO_TEXT = "Hello Easter! what? ";
+	public static final String SAY_HELLO_TEXT = "Hello Easter! what? show add ";
 	
 	public String sayHi(String text);
 
