61becb60d7057dd9fcd8f21f78bc8fbf39925bb8
Julia <Julia@192.168.1.70>
2012-03-18 21:08:16 -0500

    third commit to test push on eclipse

--- spring-webservices-samples/annotated-action-endpoint/src/main/java/com/marvelution/samples/spring/ws/annotated/action/endpoint/HelloWorldImpl.java
+++ spring-webservices-samples/annotated-action-endpoint/src/main/java/com/marvelution/samples/spring/ws/annotated/action/endpoint/HelloWorldImpl.java
@@ -1,7 +1,7 @@
 package com.marvelution.samples.spring.ws.annotated.action.endpoint;
 
 public class HelloWorldImpl implements HelloWorld {
-	
+	//how to push using eclipse
 	public String sayHello(String text) {
 		return SAY_HELLO_TEXT + " [" + text + "]";
 	}
